cfb100c35c4acd0c76ac1857828cb01196f71d42
VitMexia <mexia.vitor@gmail>
2018-12-04 20:58:32 +0000

    Save and Read

--- app/src/main/java/draw/g12/li21n/poo/isel/pt/draw/Model/DrawProvider.java
+++ app/src/main/java/draw/g12/li21n/poo/isel/pt/draw/Model/DrawProvider.java
@@ -9,20 +9,20 @@ public class DrawProvider implements DrawType {
 
     @Override
     public Figure getDrawable(String type, Point point) {
-        type = this.getClass().getPackage().getName() + "." + type; // convert to canonical name
+            type = this.getClass().getPackage().getName() + "." + type; // convert to canonical name
 
-        Constructor<?> constructor = null;
-        try {
-            constructor = Class.forName(type).getConstructor(point.getClass());
-        } catch (NoSuchMethodException | ClassNotFoundException e) {
-            Log.e("Draw", "Error loading constructor for class " + type, e);
-        }
-        Object obj = null;
-        try {
-            obj = constructor.newInstance(point);
-        } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            Log.e("Draw", "Error instantiating " + type, e);
-        }
-        return (Figure) obj;
+            Constructor<?> constructor = null;
+            try {
+                constructor = Class.forName(type).getConstructor(point.getClass());
+            } catch (NoSuchMethodException | ClassNotFoundException e) {
+                Log.e("Draw", "Error loading constructor for class " + type, e);
+            }
+            Object obj = null;
+            try {
+                obj = constructor.newInstance(point);
+            } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+                Log.e("Draw", "Error instantiating " + type, e);
+            }
+            return (Figure) obj;
     }
 }
